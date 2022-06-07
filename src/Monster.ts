61fda8caf62b7d7e4b9e356f374f9ec9bce9b918
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-06-07 16:17:51 -0300

    Readme Update

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;   
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  recieveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.recieveDamage(this._strength);
+  }
+}
